7ac7fbb46668254bad5cb7290786122f6147a5e9
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2023-05-25 20:17:52 -0700

    Complete input functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("form");
+const password = document.getElementById("password");
+const confirm = document.getElementById("confirm");
+
+confirm.addEventListener("input", (e) => {
+    if (password.value === confirm.value) {
+        confirm.setCustomValidity("");
+    }
+    else {
+        confirm.setCustomValidity("Passwords must match.")
+    }
+});
